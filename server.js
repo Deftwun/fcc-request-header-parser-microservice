5df08990213b869b2ae2114df455771c52fbfda4
Deftwun <jwake315@gmail.com>
2016-01-17 17:24:16 -0500

    useragent dpendency

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static("client"));
+app.get("/",function(req,res){
+  console.log('User-Agent: ' + req.headers['user-agent']);
+});
 
 app.listen(port, function () {
   console.log('request-header-response-parser running on port ' + port);
